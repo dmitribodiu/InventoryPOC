3532ac403a990f0962fc2599e51d828b4dd7fdfc
Bodiu <dmitri.bodiu@katoennatie.com>
2020-11-30 17:23:14 +0200

    schema

---
+++ Src/Goods/PieceGoodsSpecification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Goods
+{
+    public class PieceGoodsSpecification
+    {
+        public Guid? PieceId { get; set; }
+        public Product Product { get; set; }
+        public PieceGoodsProperties  PieceGoodsProperties { get; set; }
+    }
+
+    public class PieceGoodsProperties
+    {
+        public PieceGoodsDimensions PieceGoodsDimensions { get; set; }
+        public string Grammage { get; set; }
+        public string Quality { get; set; }
+        public string Standart { get; set; }
+    }
+
+    public class PieceGoodsDimensions
+    {
+        public string Length { get; set; }
+        public string Thickness { get; set; }
+        public string Diameter { get; set; }
+        public string Width { get; set; }
+        public string StripLength { get; set; }
+    }
+}
---
+++ Src/Goods/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Goods
+{
+    public enum UnitOfMeasurement
+    {
+        Kg,
+        Each
+    }
+
+    public enum ProductHandling
+    {
+        InBulk,
+        InPieces
+    }
+
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Article
+    {
+        public Product Product { get; set; }
+        public string Configuration { get; set; }
+    }
+
+    public class GoodsSpecification
+    {
+        public BulkGoodsSpecification BulkGoodsSpecification { get; set; }
+        public PackedGoodsSpecification PackedGoodsSpecification { get; set; }
+        public BundledGoodsSpecification BundledGoodsSpecification { get; set; }
+        public PieceGoodsSpecification PieceGoodsSpecification { get; set; }
+
+    }
+
+
+
+    public class BundledGoodsSpecification
+    {
+    }
+
+    public class PackedGoodsSpecification
+    {
+    }
+
+    public class BulkGoodsSpecification
+    {
+    }
+}
